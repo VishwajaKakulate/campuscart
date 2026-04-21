660c84a3ec853d952305465c2d3156c03165d33e
vishwaja <vishwajakakulate@gmail.com>
2026-04-21 15:45:08 +0530

    fix port for render

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("CampusCart Backend Running 🚀");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const mongoose = require("mongoose");
 
